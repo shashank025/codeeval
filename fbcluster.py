38a7d97a6b12ebfd6c935394774ef935245ed8b3
Shashank Ramaprasad <shashank.ramaprasad@gmail.com>
2015-01-05 11:56:09 -0800

    code golf: simplify logic

--- fbcluster.py
+++ fbcluster.py
@@ -4,44 +4,31 @@ import fileinput
 pattern = re.compile('\s+')
 
 edges = set()
-
 def is_tight_with(cluster, node):
     return all((v, node) in edges and (node, v) in edges for v in cluster)
 
 for line in fileinput.input():
     from_u, to_u = pattern.split(line.strip())[-2:]
-    edge = (from_u, to_u)
-    edges.add(edge)
-
-# print edges
+    edges.add((from_u, to_u))
 
 clusters = []
 for i, (a, b) in enumerate(edges):
     rev_edge = (b, a)
-    if not rev_edge in edges:  # relationship is one way, so move on...
+    if not rev_edge in edges:
         continue
-    # a->b and b->a are both valid,
-    # so a and b are eligible to be clustered.
-
+    # a<->b is a strong edge: a and b are eligible to be clustered.
     # initialize clusters when you see the first edge
     new_cluster = False
     if not clusters:
         new_cluster = True
-
     for cluster in clusters:
         if a in cluster and b in cluster:
             continue
-        if a in cluster and b not in cluster:
-            if is_tight_with(cluster, b):
-                cluster.add(b)
-            else:
-                new_cluster = True
-        elif b in cluster and a not in cluster:
-            if is_tight_with(cluster, a):
-                cluster.add(a)
-            else:
-                new_cluster = True
-        else: # neither a nor b in any existing clusters
+        if a in cluster and is_tight_with(cluster, b):
+            cluster.add(b)
+        elif b in cluster and is_tight_with(cluster, a):
+            cluster.add(a)
+        else:
             new_cluster = True
     if new_cluster:
         cluster = set([a, b])
--- geninput.py
+++
@@ -1,3 +0,0 @@
-from random import randint
-for i in range(10000000):
-    print randint(1, 1000000000000)
